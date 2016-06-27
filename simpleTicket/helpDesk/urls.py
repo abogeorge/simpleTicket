c68d6dab3edab8807aea4db71aab9f3d2c53bc6b
George Abordioaie <abordioaie.george@yahoo.com>
2016-06-27 13:23:02 +0300

    Help Desk - Closed Tickets

--- simpleTicket/helpDesk/urls.py
+++ simpleTicket/helpDesk/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
-  # Change subalterns open orders status
+  # View all employees
   url(r'^employees/$', views.employees, name='employees'),
+  # View all closed tickets
+  url(r'^closed_tickets/$', views.closed_tickets, name='closed_tickets'),
 ]
--- simpleTicket/helpDesk/views.py
+++ simpleTicket/helpDesk/views.py
@@ -7,15 +7,32 @@ from django.template import RequestContext
 from siteEngine.validation_utils import *
 from siteEngine.entities_utils import *
 
+# Returns the user type for security purposes
+def __get_user_role(request):
+     # Retrieving user type
+    user = request.user
+    user_profile = user.userprofile
+    user_role = get_user_type(user_profile)
+    return user_role
+
 # All Employees Page
 @login_required
 def employees(request):
     # Retrieving user type
-    user = request.user
-    user_profile = user.userprofile
-    user_role = get_user_type(user_profile)
+    user_role = __get_user_role(request)
     # Retrieving all Users Profile
     users_profile = get_employees()
     if len(users_profile) == 0:
         users_profile = False
     return render(request, "employees.html", {'employees':users_profile, 'user_role':user_role})
+
+# All closed tickets page
+@login_required
+def closed_tickets(request):
+    # Retrieving user type
+    user_role = __get_user_role(request)
+    # Retrieving all Users Profile
+    tickets = Ticket.objects.filter(status = 3)
+    if len(tickets) == 0:
+        tickets = False
+    return render(request, "closed_tickets.html", {'tickets':tickets, 'user_role':user_role})
